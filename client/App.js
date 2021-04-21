1fff053a0028a9f910a03042d445f863648eb7ed
Simen H <simenmelo@gmail.com>
2021-04-21 20:36:25 +0200

    refactor: move bar components from App to Main

--- client/App.js
+++ client/App.js
@@ -3,8 +3,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text } from 'react-native';
 import { useFonts } from 'expo-font';
 
-import TopBar from './components/bars/TopBar.component';
-import BottomBar from './components/bars/BottomBar.component';
 import Main from './containers/main/Main.container';
 
 export default function App() {
@@ -21,9 +19,7 @@ export default function App() {
   } else {
     return (
       <View style={styles.container}>
-        <TopBar />
         <Main />
-        <BottomBar />
         <StatusBar style="auto" />
       </View>
     );
@@ -33,8 +29,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#BAA47A',
-    alignItems: 'center',
-    flexDirection: 'column',
+    backgroundColor: '#BAA47A'
   }
 });
