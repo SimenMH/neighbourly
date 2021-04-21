8f1149f936f8fa818f08c6fbf2ef458d48a13d2c
Simen H <simenmelo@gmail.com>
2021-04-21 19:00:42 +0200

    feat(postlist): add post list with mock data

--- client/components/post-list/PostList.component.js
+++ client/components/post-list/PostList.component.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
+
+import Post from '../post/Post.component';
+import { POSTS } from './mock-data';
 
 export default function PostList (props) {
   return (
-    <Text>PostList</Text>
-  )
+    <View style={styles.container}>
+      <FlatList
+        data={POSTS}
+        renderItem={Post}
+        keyExtractor={post => post.id}
+        contentContainerStyle={{ paddingBottom: 100}}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%',
+    paddingBottom: 100,
+  },
+});
---
+++ client/components/post-list/mock-data.js
@@ -0,0 +1,42 @@
+export const POSTS = [
+  {
+    id: '0',
+    content: 'This is a post lololol'
+  },
+  {
+    id: '1',
+    content: 'This is a post lololol'
+  },
+  {
+    id: '2',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque eget dui nec tempor. Duis lacinia magna vel orci ultrices, eget blandit ante tempor. Proin sit amet leo odio. Vivamus lobortis id libero sed auctor. Suspendisse quis velit at lacus mattis convallis. Praesent non eros et nulla sodales bibendum nec at lectus. Vivamus in ligula ut enim fringilla convallis. Pellentesque dictum vel ipsum sed dapibus. Proin et ultricies metus. Nullam sit amet turpis et arcu suscipit mattis quis dapibus mauris. Vestibulum efficitur efficitur leo, sit amet convallis augue tempor ut. Aliquam vitae ipsum lectus. Vivamus sapien elit, auctor neca.'
+  },
+  {
+    id: '3',
+    content: 'This is a post lololol'
+  },
+  {
+    id: '4',
+    content: 'This is a post lololol'
+  },
+  {
+    id: '5',
+    content: 'This is a post lololol'
+  },
+  {
+    id: '6',
+    content: 'This is a post lololol'
+  },
+  {
+    id: '7',
+    content: 'This is a post lololol'
+  },
+  {
+    id: '8',
+    content: 'This is a post lololol'
+  },
+  {
+    id: '9',
+    content: 'This is a post lololol'
+  },
+]
