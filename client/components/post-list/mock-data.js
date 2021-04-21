00f136883645dcb729108cad83916568c29c1bd9
Simen H <simenmelo@gmail.com>
2021-04-21 20:21:09 +0200

    refactor: update mock data

--- client/components/post-list/mock-data.js
+++ client/components/post-list/mock-data.js
@@ -9,42 +9,46 @@
 export const POSTS = [
   {
     id: '0',
-    content: 'This is a post lololol'
+    content: 'This is a post'
   },
   {
     id: '1',
-    content: 'This is a post lololol'
+    content: 'This is a post'
+  },
+  {
+    id: '3',
+    content: 'This is a post'
+  },
+  {
+    id: '4',
+    content: 'This is a post'
+  },
+  {
+    id: '5',
+    content: 'This is a post'
+  },
+  {
+    id: '6',
+    content: 'This is a post'
+  },
+  {
+    id: '7',
+    content: 'This is a post'
   },
   {
     id: '2',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque eget dui nec tempor. Duis lacinia magna vel orci ultrices, eget blandit ante tempor. Proin sit amet leo odio. Vivamus lobortis id libero sed auctor. Suspendisse quis velit at lacus mattis convallis. Praesent non eros et nulla sodales bibendum nec at lectus. Vivamus in ligula ut enim fringilla convallis. Pellentesque dictum vel ipsum sed dapibus. Proin et ultricies metus. Nullam sit amet turpis et arcu suscipit mattis quis dapibus mauris. Vestibulum efficitur efficitur leo, sit amet convallis augue tempor ut. Aliquam vitae ipsum lectus. Vivamus sapien elit, auctor neca.'
   },
   {
-    id: '3',
-    content: 'This is a post lololol'
-  },
-  {
-    id: '4',
-    content: 'This is a post lololol'
-  },
-  {
-    id: '5',
-    content: 'This is a post lololol'
-  },
-  {
-    id: '6',
-    content: 'This is a post lololol'
-  },
-  {
-    id: '7',
-    content: 'This is a post lololol'
-  },
-  {
     id: '8',
-    content: 'This is a post lololol'
+    content: 'This is a post'
   },
   {
     id: '9',
-    content: 'This is a post lololol'
+    content: 'This is a post'
+  },
+  {
+    id: '10',
+    content: 'This is a new post'
   },
 ]
