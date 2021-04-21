9fab142746ec9d5d58061390273148a9c5bfcb2c
Simen H <simenmelo@gmail.com>
2021-04-21 18:36:05 +0200

    feat: set up home container

--- client/containers/home/Home.container.js
+++ client/containers/home/Home.container.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, View, ImageBackground } from 'react-native';
+import PostList from '../../components/post-list/PostList.component';
+
+
+const corkBackground = require('../../assets/cork-texture01.jpg')
 
 export default function Home (props) {
   return (
-    <Text>Home</Text>
-  )
+    <View style={styles.container}>
+      <ImageBackground source={corkBackground} style={styles.background} />
+      <PostList />
+    </View>
+  );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%'
+  },
+  background: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    opacity: 0.1,
+  },
+})
