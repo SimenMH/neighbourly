4c9cc1a766a05b498e92e5a96b142c5227df47ba
Simen H <simenmelo@gmail.com>
2021-04-23 09:31:00 +0200

    style: remove console logs

--- server/helpers/meterDistance.js
+++ server/helpers/meterDistance.js
@@ -1,6 +1,5 @@
 // Compares two latitude and longitude coordinates and returns the distance in meters
 function distanceInMeters(pos1, pos2) {
-  console.log(pos1, pos2);
   var p = 0.017453292519943295;
   var c = Math.cos;
   var a = 0.5 - c((pos2.lat - pos1.lat) * p)/2 + 
@@ -8,7 +7,6 @@ function distanceInMeters(pos1, pos2) {
           (1 - c((pos2.lon - pos1.lon) * p))/2;
 
   const distance =  Math.floor((12742 * Math.asin(Math.sqrt(a))) * 1000);
-  console.log(distance)
   return distance;
 }
 
