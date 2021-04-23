e6856b900bce884eeaa1bb2754bc3fcde64330f3
Simen H <simenmelo@gmail.com>
2021-04-23 07:39:40 +0200

    feat: use .env for api key

--- client/containers/location-picker/MapPreview.js
+++ client/containers/location-picker/MapPreview.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Image, View, Text } from 'react-native';
+import { GOOGLE_API_KEY } from '@env';
 
 export default function MapPreview (props) {
   let mapUrl;
   if (props.location) {
-    mapUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.latitude},${props.location.longitude}&zoom=13&size=400x200&maptype=roadmap&markers=${props.location.latitude},${props.location.longitude}&key=AIzaSyC5p3Zo8r_EgO5ce_kTwRrdzCOH1hEb4EA`;
+    mapUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.latitude},${props.location.longitude}&zoom=13&size=400x200&maptype=roadmap&markers=${props.location.latitude},${props.location.longitude}&key=${GOOGLE_API_KEY}`;
   }
 
   return (
