d1b9ca299bb5c9e4e83881fd4ad2c52a7f68bd5f
Simen H <simenmelo@gmail.com>
2021-04-23 09:10:43 +0200

    feat: add measure distance helper script

---
+++ server/helpers/meterDistance.js
@@ -0,0 +1,15 @@
+// Compares two latitude and longitude coordinates and returns the distance in meters
+function distanceInMeters(pos1, pos2) {
+  console.log(pos1, pos2);
+  var p = 0.017453292519943295;
+  var c = Math.cos;
+  var a = 0.5 - c((pos2.lat - pos1.lat) * p)/2 + 
+          c(pos1.lat * p) * c(pos2.lat * p) * 
+          (1 - c((pos2.lon - pos1.lon) * p))/2;
+
+  const distance =  Math.floor((12742 * Math.asin(Math.sqrt(a))) * 1000);
+  console.log(distance)
+  return distance;
+}
+
+module.exports = { distanceInMeters };
