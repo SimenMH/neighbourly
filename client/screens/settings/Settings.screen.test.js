0ee336df3f30bb9b4ed22586a40317d29d2a52b6
Aric <aricjiang89@gmail.com>
2021-05-04 16:48:01 +0800

    test: change test name

--- client/screens/settings/Settings.screen.test.js
+++ client/screens/settings/Settings.screen.test.js
@@ -3,7 +3,7 @@ import { render, fireEvent } from '@testing-library/react-native';
 import Settings from './Settings.screen';
 
 describe('Settings component', () => {
-  test('navigates to LocationPicker screen component', () => {
+  it('should say Change Location', () => {
     const navigation = {};
     navigation.navigate = jest.fn();
     const { getByText } = render(<Settings navigation={navigation} />);
